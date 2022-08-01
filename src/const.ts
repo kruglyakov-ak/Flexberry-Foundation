e0895a9e293e4da04ac2f6ad2d3a9ac6a62c5bfd
Aleksandr <aleksandr.k.kruglyakov@yandex.ru>
2022-08-01 17:40:11 +0300

    add constants file

---
+++ src/const.ts
@@ -0,0 +1,9 @@
+const enum FilterNames {
+  All = "all",
+  NonTransfer = "non-transfer",
+  Transfer1 = "1-transfer",
+  Transfer2 = "2-transfer",
+  Transfer3 = "3-transfer",
+}
+
+export { FilterNames };
