48a5eda1a3f5c5b1957e67cadb3e88318ed1fa72
Aleksandr <aleksandr.k.kruglyakov@yandex.ru>
2022-08-01 19:08:48 +0300

    create SortTabs component

---
+++ src/components/main-page/components/main/components/offers/components/sort-tabs/sort-tabs.tsx
@@ -0,0 +1,55 @@
+import "./style.css";
+import { SyntheticEvent } from "react";
+import { SortTabNames } from "../../../../../../../../const";
+
+type SortTabsProps = {
+  sortType: SortTabNames;
+  handleSortButtonClick: ({
+    currentTarget,
+  }: SyntheticEvent<HTMLButtonElement>) => void;
+};
+
+function SortTabs({ sortType, handleSortButtonClick }: SortTabsProps) {
+  return (
+    <ul className="sort-tabs">
+      <li className="sort-tabs__item">
+        <button
+          className={`sort-tabs__button ${
+            sortType === SortTabNames.Cost ? "sort-tabs__button--active" : ""
+          }`}
+          name={SortTabNames.Cost}
+          type="button"
+          onClick={handleSortButtonClick}
+        >
+          Самый дешевый
+        </button>
+      </li>
+      <li className="sort-tabs__item">
+        <button
+          className={`sort-tabs__button ${
+            sortType === SortTabNames.Speed ? "sort-tabs__button--active" : ""
+          }`}
+          type="button"
+          name={SortTabNames.Speed}
+          onClick={handleSortButtonClick}
+        >
+          Самый быстрый
+        </button>
+      </li>
+      <li className="sort-tabs__item">
+        <button
+          className={`sort-tabs__button ${
+            sortType === SortTabNames.Optimal ? "sort-tabs__button--active" : ""
+          }`}
+          type="button"
+          name={SortTabNames.Optimal}
+          onClick={handleSortButtonClick}
+        >
+          Оптимальный
+        </button>
+      </li>
+    </ul>
+  );
+}
+
+export default SortTabs;
--- src/components/main-page/components/main/main.tsx
+++ src/components/main-page/components/main/main.tsx
@@ -1,6 +1,7 @@
-import { useState, ChangeEvent } from "react";
-import { FilterNames } from "../../../../const";
+import { useState, ChangeEvent, SyntheticEvent } from "react";
+import { FilterNames, SortTabNames } from "../../../../const";
 import Filters from "./components/filters/filters";
+import Offers from "./components/offers/offers";
 import "./style.css";
 
 function Main() {
@@ -9,6 +10,8 @@ function Main() {
     FilterNames.Transfer1,
     FilterNames.Transfer2,
   ]);
+
+  const [sortType, setSortType] = useState<SortTabNames>(SortTabNames.Cost);
 
   const filterCheckboxChangeHandler = ({
     target,
@@ -19,6 +22,12 @@ function Main() {
       return;
     }
     filters.push(target.id as FilterNames);
+  };
+
+  const sortButtonClickHandler = ({
+    currentTarget,
+  }: SyntheticEvent<HTMLButtonElement>) => {
+    setSortType(currentTarget.name as SortTabNames);
   };
 
   return (
@@ -32,277 +41,10 @@ function Main() {
         handleFilterCheckboxChange={filterCheckboxChangeHandler}
       />
 
-      <section className="offers">
-        <ul className="sort-tabs">
-          <li className="sort-tabs__item">
-            <button
-              className="sort-tabs__button sort-tabs__button--active"
-              type="button"
-            >
-              Самый дешевый
-            </button>
-          </li>
-          <li className="sort-tabs__item">
-            <button className="sort-tabs__button" type="button">
-              Самый быстрый
-            </button>
-          </li>
-          <li className="sort-tabs__item">
-            <button className="sort-tabs__button" type="button">
-              Оптимальный
-            </button>
-          </li>
-        </ul>
-
-        <article className="ticket">
-          <div className="ticket-header">
-            <p className="ticket-header__price">13 400 Р</p>
-            <img
-              className="ticket-header__aircompany-logo"
-              src="http://pics.avs.io/110/36/S7.png"
-              alt="airline logo"
-              width="110"
-              height="36"
-            />
-          </div>
-
-          <div className="ticket-body">
-            <div className="route">
-              <div className="column">
-                <div className="column__title">MOW – HKT</div>
-                <div className="column__content">10:45 – 08:00</div>
-              </div>
-
-              <div className="column">
-                <div className="column__title">В пути</div>
-                <div className="column__content">21ч 15м</div>
-              </div>
-
-              <div className="column">
-                <div className="column__title">2 пересадки</div>
-                <div className="column__content">HKG, JNB</div>
-              </div>
-            </div>
-
-            <div className="route">
-              <div className="column">
-                <div className="column__title">MOW – HKT</div>
-                <div className="column__content">11:20 – 00:50</div>
-              </div>
-
-              <div className="column">
-                <div className="column__title">В пути</div>
-                <div className="column__content">13ч 30м</div>
-              </div>
-
-              <div className="column">
-                <div className="column__title">1 пересадка</div>
-                <div className="column__content">HKG</div>
-              </div>
-            </div>
-          </div>
-        </article>
-
-        <article className="ticket">
-          <div className="ticket-header">
-            <p className="ticket-header__price">13 400 Р</p>
-            <img
-              className="ticket-header__aircompany-logo"
-              src="http://pics.avs.io/110/36/S7.png"
-              alt="airline logo"
-              width="110"
-              height="36"
-            />
-          </div>
-
-          <div className="ticket-body">
-            <div className="route">
-              <div className="column">
-                <div className="column__title">MOW – HKT</div>
-                <div className="column__content">10:45 – 08:00</div>
-              </div>
-
-              <div className="column">
-                <div className="column__title">В пути</div>
-                <div className="column__content">21ч 15м</div>
-              </div>
-
-              <div className="column">
-                <div className="column__title">2 пересадки</div>
-                <div className="column__content">HKG, JNB</div>
-              </div>
-            </div>
-
-            <div className="route">
-              <div className="column">
-                <div className="column__title">MOW – HKT</div>
-                <div className="column__content">11:20 – 00:50</div>
-              </div>
-
-              <div className="column">
-                <div className="column__title">В пути</div>
-                <div className="column__content">13ч 30м</div>
-              </div>
-
-              <div className="column">
-                <div className="column__title">1 пересадка</div>
-                <div className="column__content">HKG</div>
-              </div>
-            </div>
-          </div>
-        </article>
-
-        <article className="ticket">
-          <div className="ticket-header">
-            <p className="ticket-header__price">13 400 Р</p>
-            <img
-              className="ticket-header__aircompany-logo"
-              src="http://pics.avs.io/110/36/S7.png"
-              alt="airline logo"
-              width="110"
-              height="36"
-            />
-          </div>
-
-          <div className="ticket-body">
-            <div className="route">
-              <div className="column">
-                <div className="column__title">MOW – HKT</div>
-                <div className="column__content">10:45 – 08:00</div>
-              </div>
-
-              <div className="column">
-                <div className="column__title">В пути</div>
-                <div className="column__content">21ч 15м</div>
-              </div>
-
-              <div className="column">
-                <div className="column__title">2 пересадки</div>
-                <div className="column__content">HKG, JNB</div>
-              </div>
-            </div>
-
-            <div className="route">
-              <div className="column">
-                <div className="column__title">MOW – HKT</div>
-                <div className="column__content">11:20 – 00:50</div>
-              </div>
-
-              <div className="column">
-                <div className="column__title">В пути</div>
-                <div className="column__content">13ч 30м</div>
-              </div>
-
-              <div className="column">
-                <div className="column__title">1 пересадка</div>
-                <div className="column__content">HKG</div>
-              </div>
-            </div>
-          </div>
-        </article>
-
-        <article className="ticket">
-          <div className="ticket-header">
-            <p className="ticket-header__price">13 400 Р</p>
-            <img
-              className="ticket-header__aircompany-logo"
-              src="http://pics.avs.io/110/36/S7.png"
-              alt="airline logo"
-              width="110"
-              height="36"
-            />
-          </div>
-
-          <div className="ticket-body">
-            <div className="route">
-              <div className="column">
-                <div className="column__title">MOW – HKT</div>
-                <div className="column__content">10:45 – 08:00</div>
-              </div>
-
-              <div className="column">
-                <div className="column__title">В пути</div>
-                <div className="column__content">21ч 15м</div>
-              </div>
-
-              <div className="column">
-                <div className="column__title">2 пересадки</div>
-                <div className="column__content">HKG, JNB</div>
-              </div>
-            </div>
-
-            <div className="route">
-              <div className="column">
-                <div className="column__title">MOW – HKT</div>
-                <div className="column__content">11:20 – 00:50</div>
-              </div>
-
-              <div className="column">
-                <div className="column__title">В пути</div>
-                <div className="column__content">13ч 30м</div>
-              </div>
-
-              <div className="column">
-                <div className="column__title">1 пересадка</div>
-                <div className="column__content">HKG</div>
-              </div>
-            </div>
-          </div>
-        </article>
-
-        <article className="ticket">
-          <div className="ticket-header">
-            <p className="ticket-header__price">13 400 Р</p>
-            <img
-              className="ticket-header__aircompany-logo"
-              src="http://pics.avs.io/110/36/S7.png"
-              alt="airline logo"
-              width="110"
-              height="36"
-            />
-          </div>
-
-          <div className="ticket-body">
-            <div className="route">
-              <div className="column">
-                <div className="column__title">MOW – HKT</div>
-                <div className="column__content">10:45 – 08:00</div>
-              </div>
-
-              <div className="column">
-                <div className="column__title">В пути</div>
-                <div className="column__content">21ч 15м</div>
-              </div>
-
-              <div className="column">
-                <div className="column__title">2 пересадки</div>
-                <div className="column__content">HKG, JNB</div>
-              </div>
-            </div>
-
-            <div className="route">
-              <div className="column">
-                <div className="column__title">MOW – HKT</div>
-                <div className="column__content">11:20 – 00:50</div>
-              </div>
-
-              <div className="column">
-                <div className="column__title">В пути</div>
-                <div className="column__content">13ч 30м</div>
-              </div>
-
-              <div className="column">
-                <div className="column__title">1 пересадка</div>
-                <div className="column__content">HKG</div>
-              </div>
-            </div>
-          </div>
-        </article>
-
-        <button type="button" className="more-button">
-          Показать еще 5 билетов!
-        </button>
-      </section>
+      <Offers
+        sortType={sortType}
+        handleSortButtonClick={sortButtonClickHandler}
+      />
     </main>
   );
 }
