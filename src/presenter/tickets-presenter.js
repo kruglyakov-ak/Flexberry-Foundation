e6714051f7dcaa4f2f0ab9a1db2daf3865f0d370
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-08-07 22:31:12 +0300

    add logic add event listeners in Abstract class

--- src/presenter/tickets-presenter.js
+++ src/presenter/tickets-presenter.js
@@ -10,48 +10,65 @@ const TICKETS_COUNT_PER_STEP = 5;
 
 export default class TicketsPresenter {
   constructor(tickets) {
-    this.tickets = tickets;
+    this._tickets = tickets;
+    this._renderedTicketsCount = TICKETS_COUNT_PER_STEP;
+
+    this._offersComponent = new OffersView();
+    this._ticketsListComponent = new TicketsListView();
+    this._moreButtonComponent = null;
+
+    this._handleMoreButtonClick = this._handleMoreButtonClick.bind(this);
   }
-  offersComponent = new OffersView();
-  ticketsListComponent = new TicketsListView();
 
   init = (ticketsContainer) => {
     this.ticketsContainer = ticketsContainer;
 
     render(new FiltersView(), this.ticketsContainer);
-    render(this.offersComponent, this.ticketsContainer);
-    render(new SortView(), this.offersComponent.getElement());
+    render(this._offersComponent, this.ticketsContainer);
+    render(new SortView(), this._offersComponent.getElement());
 
-    render(this.ticketsListComponent, this.offersComponent.getElement());
+    render(this._ticketsListComponent, this._offersComponent.getElement());
 
-    this.tickets
+    this._tickets
       .slice(0, TICKETS_COUNT_PER_STEP)
       .forEach((ticket) =>
-        render(new TicketView(ticket), this.ticketsListComponent.getElement())
+        render(new TicketView(ticket), this._ticketsListComponent.getElement())
       );
 
-    if (this.tickets.length > TICKETS_COUNT_PER_STEP) {
-      let renderTemplateTicketsCount = TICKETS_COUNT_PER_STEP;
-
-      render(new MoreButtonView(), this.offersComponent.getElement());
-
-      const moreButtonElement = document.querySelector(".more-button");
-
-      moreButtonElement.addEventListener("click", (evt) => {
-        evt.preventDefault();
-
-        this.tickets
-        .slice(renderTemplateTicketsCount, renderTemplateTicketsCount + TICKETS_COUNT_PER_STEP)
-        .forEach((ticket) =>
-          render(new TicketView(ticket), this.ticketsListComponent.getElement())
-        );
-
-        renderTemplateTicketsCount += TICKETS_COUNT_PER_STEP;
-
-        if (renderTemplateTicketsCount >= this.tickets.length) {
-          moreButtonElement.remove();
-        }
-      });
+    if (this._tickets.length > TICKETS_COUNT_PER_STEP) {
+      this._renderMoreButton();
     }
   };
+
+  _handleMoreButtonClick() {
+    const ticketsCount = this._tickets.length;
+    const increasedRenderedTicketsCount = Math.min(
+      ticketsCount,
+      this._renderedTicketsCount + TICKETS_COUNT_PER_STEP
+    );
+
+    this._tickets
+      .slice(this._renderedTicketsCount, increasedRenderedTicketsCount)
+      .forEach((ticket) => {
+        render(new TicketView(ticket), this._ticketsListComponent.getElement());
+      });
+
+    this._renderedTicketsCount = increasedRenderedTicketsCount;
+
+    if (this._renderedTicketsCount >= ticketsCount) {
+      remove(this._moreButtonComponent);
+    }
+  }
+
+  _renderMoreButton() {
+    if (this._moreButtonComponent !== null) {
+      this._moreButtonComponent = null;
+    }
+
+    this._moreButtonComponent = new MoreButtonView();
+
+    render(this._moreButtonComponent, this._offersComponent.getElement());
+
+    this._moreButtonComponent.setClickHandler(this._handleMoreButtonClick);
+  }
 }
--- src/view/more-button.js
+++ src/view/more-button.js
@@ -1,13 +1,28 @@
 import AbstractView from "./abstract";
 
-const createMoreButtonTemplate = () => (`
+const createMoreButtonTemplate = () => `
   <button type="button" class="more-button">
     Показать еще 5 билетов!
   </button>
-`);
+`;
 
 export default class MoreButtonView extends AbstractView {
+  constructor() {
+    super();
+    this._clickHandler = this._clickHandler.bind(this);
+  }
+
   getTemplate() {
     return createMoreButtonTemplate();
+  }
+
+  _clickHandler(evt) {
+    evt.preventDefault();
+    this._callback.click();
+  }
+
+  setClickHandler(callback) {
+    this._callback.click = callback;
+    this.getElement().addEventListener("click", this._clickHandler);
   }
 }
