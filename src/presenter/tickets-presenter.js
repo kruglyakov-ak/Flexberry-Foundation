563dbd089b1a37b59be71be1ec10d5f75c888c37
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-08-08 00:02:15 +0300

    realaised filter logic

--- src/presenter/tickets-presenter.js
+++ src/presenter/tickets-presenter.js
@@ -4,7 +4,7 @@ import OffersView from "../view/offers";
 import SortView from "../view/sort";
 import TicketView from "../view/ticket";
 import TicketsListView from "../view/tickets-list";
-import { render } from "../utils";
+import { render, remove, filterTickets } from "../utils";
 
 const TICKETS_COUNT_PER_STEP = 5;
 
@@ -14,9 +14,10 @@ export default class TicketsPresenter {
     this._filters = filters;
     this._renderedTicketsCount = TICKETS_COUNT_PER_STEP;
 
-    this._offersComponent = new OffersView();
-    this._ticketsListComponent = new TicketsListView();
     this._filtersComponent = null;
+    this._offersComponent = null;
+    this._sortComponent = null;
+    this._ticketsListComponent = null;
     this._moreButtonComponent = null;
 
     this._handleFiltersChange = this._handleFiltersChange.bind(this);
@@ -26,35 +27,58 @@ export default class TicketsPresenter {
   init = (ticketsContainer) => {
     this._ticketsContainer = ticketsContainer;
 
-    this._renderFilters();
+    this._renderFiltersComponent();
+    this._renderOffersComponent();
+  };
+
+  _handleFiltersChange(evt) {
+    if (this._filters.includes(evt.target.name)) {
+      const index = this._filters.findIndex(
+        (filter) => filter === evt.target.name
+      );
+      this._filters.splice(index, 1);
+    } else {
+      this._filters.push(evt.target.name);
+    }
+    remove(this._offersComponent);
+    this._renderOffersComponent();
+    console.log(this._filters);
+  }
+
+  _renderFiltersComponent() {
+    this._filtersComponent = new FiltersView(this._filters);
+    render(this._filtersComponent, this._ticketsContainer);
+    this._filtersComponent.setChangeHandler(this._handleFiltersChange);
+  }
+
+  _renderOffersComponent() {
+    if (this._offersComponent !== null) {
+      this._offersComponent = null;
+    }
+
+    this._offersComponent = new OffersView();
     render(this._offersComponent, this._ticketsContainer);
-    render(new SortView(), this._offersComponent.getElement());
+    this._renderSortComponent();
+    this._renderTiketsListComponent();
 
+    if (this._tickets.length > TICKETS_COUNT_PER_STEP) {
+      this._renderMoreButtonComponent();
+    }
+  }
+
+  _renderSortComponent() {
+    this._sortComponent = new SortView();
+    render(this._sortComponent, this._offersComponent.getElement());
+  }
+
+  _renderTiketsListComponent() {
+    this._ticketsListComponent = new TicketsListView();
     render(this._ticketsListComponent, this._offersComponent.getElement());
-
-    this._tickets
+    filterTickets(this._tickets, this._filters)
       .slice(0, TICKETS_COUNT_PER_STEP)
       .forEach((ticket) =>
         render(new TicketView(ticket), this._ticketsListComponent.getElement())
       );
-
-    if (this._tickets.length > TICKETS_COUNT_PER_STEP) {
-      this._renderMoreButton();
-    }
-  };
-
-  _handleFiltersChange(evt) {
-    console.log(evt.target);
-  }
-
-  _renderFilters() {
-    if (this._filtersComponent !== null) {
-      this._filtersComponent = null;
-    }
-
-    this._filtersComponent = new FiltersView(this._filters);
-    render(this._filtersComponent, this._ticketsContainer);
-    this._filtersComponent.setChangeHandler();
   }
 
   _handleMoreButtonClick() {
@@ -77,15 +101,9 @@ export default class TicketsPresenter {
     }
   }
 
-  _renderMoreButton() {
-    if (this._moreButtonComponent !== null) {
-      this._moreButtonComponent = null;
-    }
-
+  _renderMoreButtonComponent() {
     this._moreButtonComponent = new MoreButtonView();
-
     render(this._moreButtonComponent, this._offersComponent.getElement());
-
     this._moreButtonComponent.setClickHandler(this._handleMoreButtonClick);
   }
 }
