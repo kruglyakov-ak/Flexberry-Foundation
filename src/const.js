0440db9bc502cece899517b6301b9f05d47c5214
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-08-07 23:04:35 +0300

    add filters names constant

---
+++ src/const.js
@@ -0,0 +1,16 @@
+const RenderPosition = {
+  BEFOREBEGIN: "beforebegin",
+  AFTERBEGIN: "afterbegin",
+  BEFOREEND: "beforeend",
+  AFTEREND: "afterend",
+};
+
+const FiltersNames = {
+  ALL: "all",
+  NON_TRANSFER: "non-transfer",
+  ONE_TRANSFER: "one_transfer",
+  TWO_TRANSFER: "two_transfer",
+  THREE_TRANSFER: "three_transfer",
+};
+
+export { RenderPosition, FiltersNames };
